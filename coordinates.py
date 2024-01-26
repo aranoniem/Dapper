42737ee3f2904f571c7d34be6d0c4e46b196c062
Paco4712 <pacovandervliet2004@gmail.com>
2024-01-26 12:41:15 +0100

    Fixed the bug in greedy that caused it to return an incorrect score. Fixed the bug in coordinates.py that caused the legend to have incorrect colors that didnt correspond to the correct routes, also made it so for connections the travel time is shown not just for the connections part of a route. I also made the offset per extra connection dependent on which railnetwork is loaded. I also added extra comments and added the map of the netherlands being loaded in the background

--- coordinates.py
+++ coordinates.py
@@ -1,25 +1,33 @@
 import csv
-import sys
-import os
 import random
+import sys
 import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch
 from matplotlib.lines import Line2D
+import geopandas as gpd
+import contextily as cx
 
 sys.path.append('code')
 from code.algorithms.greedy import GreedySearch
 from code.classes.station import Station
 
 class Railroadmap:
-    def __init__(self, name, latitude, longitude):
-        self.name = name
+    def __init__(self, level, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
         self.connections = []  # List to store connected locations
         self.trajectories = []  # List to store trajectories
-        self.color_list = []
+        self.level = level
+        
+        # stores the offset for x or y for one extra connection.
+        # the reason for the offset being different for "Holland" than for "Nationaal" is because "Holland" has a lower density 
+        # in connections so with a low offset its still visible
+        self.offset_coefficient = 0.008 if level == "Holland" else 0.016
 
     def read_locations(self, file_path):
+        """
+        read out all the connections from the csv file and store it in a dictionary that has the values mapped by place name
+        """
         locations = {}
         with open(file_path, 'r') as csv_file:
             csv_reader = csv.reader(csv_file)
@@ -32,6 +40,9 @@ class Railroadmap:
         return locations
 
     def read_connections(self, file_path):
+        """
+        read out all the connection s between stations from the csv file and store the connections in a dictionary mapped by the departure station
+        """
         connections = {}
 
         with open(file_path, 'r') as csv_file:
@@ -51,116 +62,141 @@ class Railroadmap:
                 connections[departure_station].append((arrival_station, time))
 
         return connections
-    
+        
     def generate_random_color(self):
         """
         Generates a random RGB color.
         """
         return [random.random(), random.random(), random.random()]
+        
     
-    def plot_locations(self, locations, connections):
+    def plot_locations(self, locations, connections, NL_shape):
         connection_offsets = {}
         used_reversed_connections = set()
-    
+        
+        # set the size of the subplot
+        fig, ax = plt.subplots(figsize=(10,10))
+        
+        # draw the plot of the Netherlands behind the plot of the railnetwork
+        NL_shape.plot(ax=ax, alpha=0.3, edgecolor="black", facecolor="white")
+
         plt.scatter([loc.longitude for loc in locations.values()], [loc.latitude for loc in locations.values()], marker='o', color='red')
-    
+
         for loc in locations.values():
             plt.text(loc.longitude, loc.latitude, loc.name, ha='right', va='bottom', fontsize=8)
-    
+
         # Draw lines for connections that are not part of any route
         for dep_station, destinations in connections.items():
             for dest, time in destinations:
                 connection = (dep_station, dest)
                 reversed_connection = (dest, dep_station)
-    
+
                 # Check if the reversed connection is used
                 if reversed_connection in used_reversed_connections:
                     used_reversed_connections.add(reversed_connection)
-    
+                
+                # If the connection is not part of a route
                 if connection not in self.connections and reversed_connection not in self.connections:
                     dep_coords = locations[dep_station]
                     arr_coords = locations[dest]
-    
-                    # Adjust offsets if reversed connection is used
+
+                    # Determine what type of connection "connection" is
                     offset_connection = connection if connection not in connection_offsets else reversed_connection
-    
+
                     if offset_connection not in connection_offsets:
                         connection_offsets[offset_connection] = 0
-    
+                    
                     ratio = (arr_coords.longitude - dep_coords.longitude) / (arr_coords.latitude - dep_coords.latitude)
-                    horizontal_offset = 0.008 * connection_offsets[offset_connection] if ratio <= 1 else 0
-                    vertical_offset = 0.008 * connection_offsets[offset_connection] if ratio > 1 else 0
-    
+                    horizontal_offset = self.offset_coefficient * connection_offsets[offset_connection] if ratio <= 1 else 0
+                    vertical_offset = self.offset_coefficient * connection_offsets[offset_connection] if ratio > 1 else 0
+
                     # Adjust offsets if reversed connection is used
                     if reversed_connection in used_reversed_connections:
                         connection_offsets[reversed_connection] = max(connection_offsets[reversed_connection], connection_offsets[connection])
-    
+
                     connection_offsets[offset_connection] += 1
-    
+
                     line = FancyArrowPatch((dep_coords.longitude + horizontal_offset, dep_coords.latitude + vertical_offset),
-                                            (arr_coords.longitude + horizontal_offset, arr_coords.latitude + vertical_offset),
-                                            color=(0.5, 0.5, 0.5), arrowstyle='-|>', mutation_scale=10)
+                           (arr_coords.longitude + horizontal_offset, arr_coords.latitude + vertical_offset), color=(0.5, 0.5, 0.5), arrowstyle='-|>', mutation_scale=10)
                     plt.gca().add_patch(line)
-    
-        # loop through each trajectory
+
+                    # Print time for the connection
+                    matching_times = [time for dest, time in connections.get(dep_station, []) if dest == dest]
+                    non_zero_times = [float(time) for time in matching_times if float(time) > 0]
+
+                    if non_zero_times:
+                        current_route_time = non_zero_times[0]
+                        text_x = (dep_coords.longitude + arr_coords.longitude) / 2
+                        text_y = (dep_coords.latitude + arr_coords.latitude) / 2
+                        min_text_distance = 0.002
+                        text_y = max(text_y, dep_coords.latitude + min_text_distance)
+                        plt.text(text_x, text_y, f"{current_route_time:.2f} mins", ha='center', va='center', fontsize=8, color='black')
+        
+        # create lists for the labels which store the route number, and for a list that stores the coloured lines that belong to the corresponding route labels
+        unique_color_labels = []
+        unique_color_lines_list = []  
+
+        # Loop through each trajectory
         for i, route in enumerate(self.trajectories):
             color = self.generate_random_color()
-    
-            # loop for all the connections in a route
+
+            # Using Line2D library for drawing a line in the color corresponding to the route
+            unique_color_labels.append(f'Route {i + 1}')
+
+            # Using Line2D library for drawing a line in the color corresponding to the route
+            unique_color_line = Line2D([0], [0], marker='o', color=color, label=unique_color_labels[i], markersize=5)
+            unique_color_lines_list.append(unique_color_line)  
+
+            # Loop for all the connections in a route
             for j in range(len(route) - 1):
                 departure_station = route[j]
                 arrival_location = route[j + 1]
                 dep_coords = locations[departure_station]
                 arr_coords = locations[arrival_location]
-    
+
                 connection = (departure_station, arrival_location)
                 reversed_connection = (arrival_location, departure_station)
-    
+
                 offset_connection = connection if connection in connection_offsets else reversed_connection
-    
+
                 if offset_connection not in connection_offsets:
                     connection_offsets[offset_connection] = 0
-    
+
                 ratio = (arr_coords.longitude - dep_coords.longitude) / (arr_coords.latitude - dep_coords.latitude)
-                horizontal_offset = 0.008 * connection_offsets[offset_connection] if ratio <= 1 else 0
-                vertical_offset = 0.00 * connection_offsets[offset_connection] if ratio > 1 else 0
-    
+                horizontal_offset = self.offset_coefficient * connection_offsets[offset_connection] if ratio <= 1 else 0
+                vertical_offset = self.offset_coefficient * connection_offsets[offset_connection] if ratio > 1 else 0
+                    
                 connection_offsets[offset_connection] += 1
-    
                 distance = dep_coords.get_distance(arrival_location)
-    
+
                 if not (connection in connections or reversed_connection in connections):
-                    line = FancyArrowPatch((dep_coords.longitude + horizontal_offset, dep_coords.latitude + vertical_offset),
-                                            (arr_coords.longitude + horizontal_offset, arr_coords.latitude + vertical_offset),
-                                            color=color, arrowstyle='-|>', mutation_scale=15)
-    
+                    line = FancyArrowPatch((dep_coords.longitude + horizontal_offset, dep_coords.latitude + vertical_offset), (arr_coords.longitude + horizontal_offset, arr_coords.latitude + vertical_offset),
+                    color=color, arrowstyle='-|>', mutation_scale=15)
+
                     plt.gca().add_patch(line)
-    
-                text_x = (dep_coords.longitude + arr_coords.longitude) / 2
-                text_y = (dep_coords.latitude + arr_coords.latitude) / 2
-    
-                min_text_distance = 0.002
-                text_y = max(text_y, dep_coords.latitude + min_text_distance)
-    
-                matching_times = [time for dest, time in connections.get(departure_station, []) if dest == arrival_location]
-    
-                non_zero_times = [float(time) for time in matching_times if float(time) > 0]
-    
-                if non_zero_times:
-                    current_route_time = non_zero_times[0]
-                    plt.text(text_x, text_y, f"{current_route_time:.2f} mins", ha='center', va='center', fontsize=8,
-                            color='black')
-    
-        # Combine legend labels for routes and unique colors with corresponding colors
-        unique_color_labels = [f'Route {i + 1}' for i in range(len(self.trajectories))]
-    
-        # using Line2D library for drawing a line in the color corresponding to the route
-        unique_color_patches = [Line2D([0], [0], marker='o', color=color, label=label, markersize=5) for color, label in zip([self.generate_random_color() for route in self.trajectories], unique_color_labels)]
-        plt.legend(handles=unique_color_patches, loc='lower right')
-    
-        plt.show()
-        
+
+                    # Print time for the connection
+                    matching_times = [time for dest, time in connections.get(departure_station, []) if dest == arrival_location]
+                    non_zero_times = [float(time) for time in matching_times if float(time) > 0]
+
+                    if non_zero_times:
+                        current_route_time = non_zero_times[0]
+                        text_x = (dep_coords.longitude + arr_coords.longitude) / 2
+                        text_y = (dep_coords.latitude + arr_coords.latitude) / 2
+                        min_text_distance = 0.002
+                        text_y = max(text_y, dep_coords.latitude + min_text_distance)
+                        plt.text(text_x, text_y, f"{current_route_time:.2f} mins", ha='center', va='center', fontsize=8, color='black')
+
+        # Update the legend after the loop with the list of handles
+        plt.legend(handles=unique_color_lines_list, loc='lower right')
+
+        # Set the background map 
+        cx.add_basemap(ax, crs=NL_shape.crs, source=cx.providers.OpenStreetMap.Mapnik)
+
     def set_plot_limits(self, locations):
+        """
+        find and set the edge values of the plot by determining the minimum and maximum latitude and longitude values
+        """
         min_lat = min(loc.latitude for loc in locations.values())
         max_lat = max(loc.latitude for loc in locations.values())
         min_lon = min(loc.longitude for loc in locations.values())
@@ -171,27 +207,33 @@ class Railroadmap:
         plt.gca().set_aspect('equal', adjustable='box')
 
     def main(self):
-        locations = self.read_locations('data/StationsNationaal.csv')
-        connections = self.read_connections('data/ConnectiesNationaal.csv')
-    
-        num_colors = 20  # Number of different colors for trajectories
-        greedy_search = GreedySearch.solve('Nationaal', 20, 120)
-    
+        locations = self.read_locations(f'data/Stations{self.level}.csv')
+        connections = self.read_connections(f'data/Connecties{self.level}.csv')
+
+        # stores the path to the shapefile which is used for retrieving the map of the Netherlands
+        # shapefile origin: https://www.naturalearthdata.com/downloads/50m-cultural-vectors/
+        # sources: https://stackoverflow.com/questions/65110568/how-to-add-a-real-map-as-the-background-to-a-plot, https://contextily.readthedocs.io/en/latest/intro_guide.html
+        # https://stackoverflow.com/questions/61436956/set-shape-restore-shx-config-option-to-yes-to-restore-or-create-it
+        shapefile_path = 'shapefile_for_visualisation/ne_50m_admin_0_countries.shp'
+
+        # Read the shapefile in for plotting it later
+        NL_shape = gpd.read_file(shapefile_path)
+        greedy_search = GreedySearch.solve(self.level, 7, 120)
+
         trajectories = greedy_search.trajectories
-    
+
         self.trajectories = trajectories
-    
-        plt.figure(figsize=(10, 10))
-        self.plot_locations(locations, connections)
+
+        self.plot_locations(locations, connections, NL_shape)
         self.set_plot_limits(locations)
-    
-        plt.xlabel('Longitude')
-        plt.ylabel('Latitude')
-        plt.title('Coordinate Grid of Locations in Holland with Connections and Times')
-    
+
+        plt.xlabel('Latitude')
+        plt.ylabel('Longitude')
+        plt.title(f'Coordinate Grid of Locations in {self.level} with Connections and Times')
+
         plt.grid(True)
         plt.show()
 
 if __name__ == "__main__":
-    railroad_map = Railroadmap('example', 0.0, 0.0)
+    railroad_map = Railroadmap("Nationaal", 0.0, 0.0)
     railroad_map.main()
